89cf484b27daccb085f686933a3a4e1b8e57dd1f
shivamgupta2017 <shivamgupta1430@gmail.com>
2017-10-08 12:29:59 +0530

    shviam

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 var example = angular.module('starter', ['ionic', 'ngSanitize']);
-example.run(function($ionicPlatform, $timeout) {
+example.run(function($ionicPlatform, $timeout, $http) {
     $ionicPlatform.ready(function() {
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -9,17 +9,35 @@ example.run(function($ionicPlatform, $timeout) {
         }
     });
 });
-example.controller("ExampleController", ['$scope',  function($scope) {
-    $scope.testmethod = function() {
-        onDeviceReadyTest();
+example.controller("ExampleController", ['$scope','$http',  function($scope, $http) 
+{
+    $scope.testmethod = function() 
+    {
+	    $http(
+	      {//add wallet to customer
+	        method: 'GET',
+	        url:'http://vedpay.com/api/Android.aspx?from=Customer&message=6%20WAREQ%209462929419%2010',
+	        data:''
+	      }).then(function successCallback(response) 
+	      {
+	      	var data=response.data;
+
+        	onDeviceReadyTest();
+
+	      }, function errorCallback(response) 
+	      {
+	      });
+
     }
+
+
 }]);
 // Global InAppBrowser reference
 var iabRef = null;
 
 //load start event
 function iabLoadStart(event) 
-{
+{	
 	navigator.notification.activityStart("Please Wait", "It'll only take a moment...");
    /*  if (event.url.match("https://payu.herokuapp.com/success")) {
        // iabRef.close();
@@ -45,13 +63,17 @@ function iabLoadStop(event)
             var a = getValue(values[0], 'mihpayid');
             var b = getValue(values[0], 'status');
             var c = getValue(values[0], 'unmappedstatus');
-            alert('final result :'+(a + b + c));
-            alert('final result again :'+(a + b + c));//you can capture values from return SURL
-           // navigator.notification.activityStop();
-            
-            //or
-            //incase values[0] contains result string
-            // console.log(getValue(values, 'mihpayid'))
+            alert('final result a mihpayid :'+a);
+            alert('final result b status:'+b);
+            alert('final result c unmappedstatus :'+c);
+
+            var data={
+            	a:a,
+            	b:b,
+            	c:c
+            }
+            alert('data :'+data);
+           
         });
   
  	iabRef.close();
@@ -89,11 +111,17 @@ function iabClose(event)
 // device APIs are available
 //
 function onDeviceReadyTest() 
-{
+{	
+
 	
-    iabRef = window.open('payuBiz.html', '_blank', 'location=no');
-	iabRef.addEventListener('loadstart', iabLoadStart);
-    iabRef.addEventListener('loadstop', iabLoadStop);
-    iabRef.addEventListener('loaderror', iabLoadError);
-    iabRef.addEventListener('exit', iabClose);
+        iabRef = window.open('payuBiz.html', '_blank', 'location=no');
+		iabret=iabRef.addEventListener('loadstart', iabLoadStart);
+	    iabRef.addEventListener('loadstop', iabLoadStop);
+	    iabRef.addEventListener('loaderror', iabLoadError);
+	    iabRef.addEventListener('exit', iabClose);
+
+     
+
+	//call 
+    
 }
